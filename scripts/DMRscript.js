26ed04421c5734a67c2003dbeae1cb030a0393e0
hairy-poppins <79613845+hairy-poppins@users.noreply.github.com>
2025-01-19 17:17:12 -0500

    fix

--- scripts/DMRscript.js
+++ scripts/DMRscript.js
@@ -3,5 +3,5 @@ function calc() {
     let seconds = parseFloat(document.getElementById("400s").value) + parseFloat(document.getElementById("800s").value) + parseFloat(document.getElementById("1600s").value) + parseFloat(document.getElementById("1200s").value);
 
     let totalSec = minutes + seconds;
-    document.getElementById("time").value = parseFloat(totalSec/60) + ":" + (totalSec % 60);
+    document.getElementById("time").value = Math.floor(totalSec/60) + ":" + (totalSec % 60.0).toFixed(2).padStart(5, "0");
 }
